e2854c37111c367e581a1475cb94c757c641a74b
se4019 <70657061+se4019@users.noreply.github.com>
2020-12-05 23:44:31 +0900

    14_03

---
+++ main3.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Book {
+	int number;
+	char title[20];
+};
+
+int main(int argc, char *argv[]) {
+	struct Book *p;
+	
+	p = (struct Book*)malloc(2*sizeof(struct Book)); 
+	
+	if (p==NULL) {
+		printf("memory allocation error!\n");
+		return -1;
+	}
+	
+	p->number = 1;
+	strcpy(p->title, "C programming");
+	
+	(p+1)->number = 2;
+	strcpy((p+1)->title, "Electronics");
+	
+	printf("%s %s\n", p->title, (p+1)->title);
+	
+	free(p);
+	return 0;
+}
